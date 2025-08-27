010402fb44dda7c341fddee2c66d812d58eb5b75
Garcia Pinto <150735867+Garcia-32@users.noreply.github.com>
2025-08-26 23:45:32 -0500

    Add files via upload

---
+++ ejemplo1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase2;
+
+import java.util.Date;
+
+public class ejemplo1 {
+    public static void main(String[] args) {
+    Date today = new Date();
+    System.out.printf("%tD\n", today);
+    System.out.printf("%tT\n", today);
+    }
+}
