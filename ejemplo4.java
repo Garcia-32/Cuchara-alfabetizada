4e9627059d537030a791921133526fd4bb0efd4a
Garcia Pinto <150735867+Garcia-32@users.noreply.github.com>
2025-08-28 09:58:31 -0500

    Add files via upload

---
+++ ejemplo4.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase2;
+
+/**
+ *
+ * @author user
+ */
+public class ejemplo4 {
+     public static void main(String[] args) {
+         
+         /* Math es una biblioteca matematica que permite generar salidas sealizandose operaciones*/
+         
+    System.out.println("Raíz cuadrada de 28 = " + Math.sqrt(28));
+    System.out.println("Valor absoluto de -16 = " + Math.abs(-16));
+    System.out.println("12 elevado a la potencia 6 = " + Math.pow(12, 6));
+    System.out.println("Redondear 46.78 = " + Math.round(46.78));
+    System.out.println("El menor valor entre 456 y 72 = " + Math.min(456, 72));
+    System.out.println("El mayor valor entre 456 y 72 = " + Math.max(456, 72));
+    System.out.println("Un valor aleatorio = " + Math.random());
+    
+/*Éste es un comentario de múltiples líneas
+    public class HolaUTP {
+    /**
+    * Éste es un comentario de documentación
+    * y es usado por javadoc para generar docs
+    * @param args (argumentos del sistema)
+    */
+   
+    // Éste es un comentario de una sola línea
+  
+    
+   
+    }
+}
