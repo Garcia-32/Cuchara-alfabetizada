e74252cdf2594b968d4c7bdad51f1a5e3acb62a4
Garcia Pinto <150735867+Garcia-32@users.noreply.github.com>
2025-09-02 23:54:47 -0500

    Add files via upload

---
+++ MantenimientoTecnico.java
@@ -0,0 +1,16 @@
+
+package empresadetransportes;
+
+
+public class MantenimientoTecnico {
+    private String codigo; 
+    private String nombres; 
+    private String apellidos;
+    private String direccion;
+    private String sexo;
+    private String correo;
+    private String celular;
+    private String especialidad;
+    private int tiempoServicio;
+    
+}
