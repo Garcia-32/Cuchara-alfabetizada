73c244462233d92bf815560bb5623fb3024c0c02
Garcia Pinto <150735867+Garcia-32@users.noreply.github.com>
2025-09-02 23:54:30 -0500

    Add files via upload

---
+++ MantenimientoClientes.java
@@ -0,0 +1,13 @@
+
+package empresadetransportes;
+
+
+public class MantenimientoClientes {
+    private String codigo; 
+    private String nombres; 
+    private String apellidos;
+    private String direccion;
+    private String sexo;
+    private String correo;
+    private String celular;  
+}
