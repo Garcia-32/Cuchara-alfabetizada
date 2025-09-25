ac5dac892c0b57995b0e9920100f6eb15e15ef5c
Garcia Pinto <150735867+Garcia-32@users.noreply.github.com>
2025-09-24 23:59:27 -0500

    Add files via upload

---
+++ TercerEjercicio.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea.sesión.pkg2;
+
+import java.util.Scanner;
+        
+public class TercerEjercicio {
+    public static void main (String[] args){
+        Scanner scanner = new scanner(System.in);
+            System.out.println("Ingrese)
+        }
+    }
+}
